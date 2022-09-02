699e96eab10eb0a01baa02ad1ea15c0298dd938f
Levan <levan.gelashvili@flatrocktech.com>
2022-09-02 16:56:05 +0400

    First commit

---
+++ buildSrc/src/main/java/Retrofit.kt
@@ -0,0 +1,9 @@
+object Retrofit {
+    private const val retrofitVersion = "2.9.0"
+    private const val okHttpVersion = "4.9.1"
+
+    const val retrofit: String = "com.squareup.retrofit2:retrofit:$retrofitVersion"
+
+    const val okHttp = "com.squareup.okhttp3:logging-interceptor:$okHttpVersion"
+    const val okHttpLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$okHttpVersion"
+}
---
+++ buildSrc/src/main/java/ThirdParty.kt
@@ -0,0 +1,5 @@
+object ThirdParty {
+    private const val destinationsVersion = "1.5.12-beta"
+    const val destinations = "io.github.raamcosta.compose-destinations:core:$destinationsVersion"
+    const val destinationsKsp = "io.github.raamcosta.compose-destinations:ksp:$destinationsVersion"
+}
